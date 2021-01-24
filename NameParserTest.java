b5a3f40d842406dee6bbf297973ca50d31a3297c
Hanna <74618691+HannaRaus@users.noreply.github.com>
2021-01-24 21:16:25 +0200

    Update NameParserTest.java

--- NameParserTest.java
+++ NameParserTest.java
@@ -15,3 +15,11 @@ class NameParser {
         return result.toString();
     }
 }
+public class NameParserTest {
+    public static void main(String[] args) {
+        String[] names = {"John Doe", "Bill Mask", "Nigua Joshua"};
+
+        //John, Bill, Nigua
+        System.out.println(new NameParser().parse(names));
+    }
+}
